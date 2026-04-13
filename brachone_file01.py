86d4b07f1ad49b44da2fef8de849350df35d9b36
Vadlamudi Srinath <80709335+EnthuAIExplorer@users.noreply.github.com>
2026-04-13 20:11:31 +0530

    Created a branchone_file01.py commit message 20:10
    External description of branchone_file01.py

---
+++ brachone_file01.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import math as m
+
+def function_sum(a,b): 
+  try: 
+    retrun a+b
+  except: 
+    print("Please provide a valid integers as arguments")
+  
